2f8c14bc9b8f507f108440a6fe1a8273123ebd84
Mathieu Moalic <matmoa@pm.me>
2025-04-29 14:40:44 +0200

    Set up nix flake app, remove docker

--- flake.nix
+++ flake.nix
@@ -1,9 +1,31 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
 
-  outputs = {nixpkgs, ...}: {
-    devShells.x86_64-linux.default = (import nixpkgs {system = "x86_64-linux";}).mkShell {
-      buildInputs = with nixpkgs.legacyPackages.x86_64-linux; [nodejs_23];
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+    buildFrontend = pkgs.buildNpmPackage {
+      src = ./.;
+      npmDepsHash = "sha256-7w/lpJO+W92l/YYMzAA0tqPHfGAF53jBA4XW2AaFXCo=";
+      version = "1.0.8";
+      pname = "homepage";
+    };
+  in {
+    apps.${system}.default = {
+      type = "app";
+      program = "${pkgs.writeShellScriptBin "serve-static-site" ''
+        exec ${pkgs.static-web-server}/bin/static-web-server \
+          -p 8081 \
+          -d ${buildFrontend}/lib/node_modules/homepage/build
+      ''}/bin/serve-static-site";
+    };
+
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        nodejs_23
+      ];
     };
   };
 }
